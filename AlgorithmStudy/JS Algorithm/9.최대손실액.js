1254beb50c916787a8a2be89a4026db25a13e6e0
SP-hong <hongseokpyou@gmail.com>
2023-08-23 13:31:56 +0900

    Update readme

--- AlgorithmStudy/JS Algorithm/9.최대손실액.js
+++ AlgorithmStudy/JS Algorithm/9.최대손실액.js
@@ -3,5 +3,5 @@ function solution(data) {
   data.forEach((v1, i, o) =>
     o.slice(i, -1).forEach((v2) => result.push(v1 - v2))
   );
-  return Math.max.apply(null, result);
+  return Math.max(...result); // return Math.max.apply(null, result);
 }
