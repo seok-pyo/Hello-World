c5998eecba43eca559f927fb7879963a676a6b6e
SP-hong <hongseokpyou@gmail.com>
2023-08-23 12:35:18 +0900

    최대손실액(투포인터)

---
+++ AlgorithmStudy/JS Algorithm/9.최대손실액.js
@@ -0,0 +1,7 @@
+function solution(data) {
+  let result = [];
+  data.forEach((v1, i, o) =>
+    o.slice(i, -1).forEach((v2) => result.push(v1 - v2))
+  );
+  return Math.max.apply(null, result);
+}
