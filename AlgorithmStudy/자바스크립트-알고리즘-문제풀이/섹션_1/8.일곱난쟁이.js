6138d125c9a310fa9adaf043c05c738c2ffd552b
SP-hong <hongseokpyou@gmail.com>
2023-08-24 15:02:39 +0900

    8.일곱난쟁이, splice, 투포인터

--- AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_1/8.일곱난쟁이.js
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_1/8.일곱난쟁이.js
@@ -1,20 +1,36 @@
+// let arr = [22, 7, 21, 19, 10, 15, 25, 8, 13];
+// function combinations(arr, num) {
+//   const result = [];
+
+//   if (num === 1) {
+//     return arr.map((v) => [v]);
+//   }
+//   arr.forEach((fixed, index, origin) => {
+//     const rest = origin.slice(index + 1);
+//     const combiArr = combinations(rest, num - 1);
+//     const attached = combiArr.map((v) => [fixed, ...v]);
+//     result.push(...attached);
+//   });
+//   return result;
+// }
+// function solution(arr) {
+//   return combinations(arr, 7).filter((v) => v.reduce((a, c) => a + c, 0) == 100)
+//     ? combinations(arr, 7).filter((v) => v.reduce((a, c) => a + c, 0) == 100)[0]
+//     : [];
+// }
+
+let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
+
 function solution(arr) {
-  let answer = arr;
-  let sum = answer.reduce((a, b) => a + b, 0);
-  let flag = 0;
-  for (let i = 0; i < 8; i++) {
-    for (let j = i + 1; j < 9; j++) {
-      if (sum - (answer[i] + answer[j]) == 100) {
-        answer.splice(j, 1);
-        answer.splice(i, 1);
-        flag = 1;
-        break;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr.reduce((a, c) => a + c, 0) - (arr[i] + arr[j]) === 100) {
+        arr.splice(j, 1); // splice의 경우 원본배열을 변경하기 때문에 뒤에 포인터부터 삭제해준다.
+        arr.splice(i, 1);
       }
     }
-    if (flag == 1) break;
   }
-  return answer;
+  return arr;
 }
 
-let arr = [22, 7, 21, 19, 10, 15, 25, 8, 13];
 console.log(solution(arr));
