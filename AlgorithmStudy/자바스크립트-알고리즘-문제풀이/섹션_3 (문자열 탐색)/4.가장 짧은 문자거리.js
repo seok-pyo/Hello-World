2f3b69749917376bf1027f9f938fb117cd45f867
SP-hong <hongseokpyou@gmail.com>
2023-08-28 19:15:05 +0900

    3-4. 가장 짧은 거리, 양방향 반복문 순회, 예외케이스 추가

--- AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_3 (문자열 탐색)/4.가장 짧은 문자거리.js
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_3 (문자열 탐색)/4.가장 짧은 문자거리.js
@@ -1,14 +1,44 @@
-function solution(str, n) {
+function solution2(str, n) {
   let idx = []; // n의 인덱스를 모두 찾고, 반복문을 통해 가장 짧은 거리를 찾는 것.
   let result = [];
   let dis = [];
-  str.split("").map((v, i) => (v === n ? idx.push(i) : false));
-  for (let i = 0; i < str.length; i++) {
-    for (num of idx) {
-      dis.push(Math.abs(num - i));
+  if (idx.length > 0) {
+    str.split("").map((v, i) => (v === n ? idx.push(i) : false));
+    for (let i = 0; i < str.length; i++) {
+      for (num of idx) {
+        dis.push(Math.abs(num - i));
+      }
+      result.push(Math.min(...dis));
+      dis = []; // 배열 초기화!
     }
-    result.push(Math.min(...dis));
-    dis = []; // 배열 초기화!
+    return result;
   }
-  return result;
+  return "빈 배열입니다.";
+}
+
+// 강의 문제 풀이
+function solution(s, t) {
+  let answer = [];
+  let p = 1000;
+  if (answer.length > 0) {
+    for (let x of s) {
+      if (x === t) {
+        p = 0;
+        answer.push(p);
+      } else {
+        p++;
+        answer.push(p);
+      }
+    }
+    p = 1000;
+    for (let i = s.length - 1; i >= 0; i--) {
+      if (s[i] === t) p = 0;
+      else {
+        p++;
+        answer[i] = Math.min(answer[i], p);
+      }
+    }
+    return answer;
+  }
+  return "빈 배열입니다.";
 }
