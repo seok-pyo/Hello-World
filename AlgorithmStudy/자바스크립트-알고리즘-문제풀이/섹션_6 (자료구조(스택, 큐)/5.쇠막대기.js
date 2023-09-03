9a9005715d0b29ce006bb9cbda80fc8534c76e5f
SP-hong <hongseokpyou@gmail.com>
2023-09-03 16:40:02 +0900

    6-5.쇠막대기, 스택, 조건 설정

---
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_6 (자료구조(스택, 큐)/5.쇠막대기.js
@@ -0,0 +1,32 @@
+let pipe = "()(((()())(())()))(())";
+
+let stack = [];
+for (let p of pipe) {
+  if (p === ")" && stack[stack.length - 1] === "(") {
+    stack.pop();
+    stack.push(1);
+    continue;
+  }
+  stack.push(p);
+}
+
+// 정답 코드
+function solution(p) {
+  let stack = [];
+  let answer = 0;
+  for (let i = 0; i < p.length; i++) {
+    if (p[i] === "(") stack.push(p[i]);
+    else {
+      if (p[i - 1] === "(") {
+        // 레이저의 끝인지, 막대의 끝인지 구분하는 것이 포인트!
+        // 두개를 구분해서 어떻게 더할 것인지를 파악해야 한다!
+        stack.pop(); // 중복 코드는 조건문 전에 올려서 먼저 실행하는 편이 좋다.
+        answer += stack.length;
+      } else {
+        stack.pop();
+        answer += 1; // 막대의 끝이라면 1을 더한다는 걸 찾아내야 한다!
+      }
+    }
+  }
+  return answer;
+}
