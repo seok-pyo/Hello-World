39513d05f841306f8846782e1b2f3748a6d7fd52
SP-hong <hongseokpyou@gmail.com>
2023-09-03 12:44:51 +0900

    6-2. 괄호 문자 제거, 강의 코드 추가(while 문으로 pop하기..!)

--- AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_6 (자료구조(스택, 큐)/2.괄호 문자 제거.js
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_6 (자료구조(스택, 큐)/2.괄호 문자 제거.js
@@ -8,3 +8,24 @@ function solution(str) {
   }
   return answer;
 }
+
+// 강의 수강 후 코드 작성
+function solution(str) {
+  let stack = [];
+  for (let x of str) {
+    if (!(x === ")")) stack.push(x);
+    else stack.splice(stack.lastIndexOf("("));
+  }
+  return stack.join("");
+}
+
+// 강사님 풀이
+function solution(s) {
+  let stack = [];
+  for (let x of s) {
+    if (x === ")") while (stack.pop() !== "(");
+    // while 문으로 pop 하기..!
+    else stack.push(x);
+  }
+  return stack.join("");
+}
