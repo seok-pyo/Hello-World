2931a5b96071449adf1130406db1a4109944bc5c
SP-hong <hongseokpyou@gmail.com>
2023-09-03 12:16:58 +0900

    6-1.올바른 괄호, 강의 코드 추가

--- AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_6 (자료구조(스택, 큐)/1.올바른 괄호.js
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_6 (자료구조(스택, 큐)/1.올바른 괄호.js
@@ -7,3 +7,19 @@ function solution(data) {
   if (stack !== 0) return "NO";
   return "YES";
 }
+
+// 강의 풀이
+function solution(s) {
+  let answer = "YES";
+  let stack = [];
+  for (let x of s) {
+    if (x === "(") stack.push(x);
+    else {
+      if (stack.length === 0) return "NO"; // 닫기 괄호가 더 많은 경우(stack에 아무것도 존재하지 않게 된다)
+      // 스택에는 여는 괄호만 들어가게 되기 때문에
+      stack.pop();
+    }
+  }
+  if (stack.length !== 0) return "NO"; // 위에 코드와 논리는 같다. 여는 괄호가 더 많은 경우
+  return answer;
+}
