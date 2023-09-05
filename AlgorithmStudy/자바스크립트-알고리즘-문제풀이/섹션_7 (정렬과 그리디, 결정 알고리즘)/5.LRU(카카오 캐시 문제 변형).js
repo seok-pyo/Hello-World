83c7d96365c24e7cd2a42fb2d90847dbe34a202d
SP-hong <hongseokpyou@gmail.com>
2023-09-05 16:29:08 +0900

    7-5. 삽입정렬 알고리즘, 강의 코드 추가

--- AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_7 (정렬과 그리디, 결정 알고리즘)/5.LRU(카카오 캐시 문제 변형).js
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_7 (정렬과 그리디, 결정 알고리즘)/5.LRU(카카오 캐시 문제 변형).js
@@ -34,3 +34,28 @@ function solution(t, n) {
   }
   return cache.join("");
 }
+
+// 삽입정렬 알고리즘 - 강의 코드 추가
+function solution(size, arr) {
+  let answer = Array.from({ length: size }, () => 0);
+  arr.forEach((v) => {
+    let pos = -1;
+    for (let i = 0; i < size; i++) if (v === answer[i]) pos = i; // hit 상황, for 문으로 판별하고,
+    // hit 된 원소의 인덱스 번호도 같이 얻을 수 있다. for 문이 다 돌떄 까지 안 내려간다. 실행 컨텍스트 기억!
+    if (pos === -1) {
+      for (let i = size - 1; i >= 1; i--) {
+        answer[i] = answer[i - 1];
+        // answer[0] = x, 중복코드 제거, 어짜피 if, else 문이 실행되고 난 이후에 answer[0] 에
+        // x를 넣으면 된다.
+      }
+    } else {
+      for (let i = pos; i >= 1; i--) {
+        answer[i] = answer[i - 1];
+        // answer[0] = x
+        // if 와 else 모두 앞에 있는 걸 뒤로 땡기는 코드, 같은 내용이니 해당 코드 실행 후에 answer[0] 에
+        // x를 넣는 건 밖으로 빼서 실행해도 된다.
+      }
+    }
+    answer[0] = v;
+  });
+}
