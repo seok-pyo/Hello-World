56cd0a670eb5453dd6f2ce83a67b658bd6afc791
SP-hong <hongseokpyou@gmail.com>
2023-09-06 12:31:53 +0900

    7-8. 회의실 배정, 강의 코드 추가, 그리디, 변수 활용

--- AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_7 (정렬과 그리디, 결정 알고리즘)/8.회의실 배정.js
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_7 (정렬과 그리디, 결정 알고리즘)/8.회의실 배정.js
@@ -1,5 +1,5 @@
 function solution(arr) {
-  answer = arr;
+  let answer = arr;
   answer.sort((a, b) => {
     if (a[1] === b[1]) return a[0] - b[0];
     else return a[1] - b[1];
@@ -9,4 +9,21 @@ function solution(arr) {
   }
   console.log(answer);
   return answer.length;
+}
+
+// 수업 문제 풀이 추가(그리디 대표 문제)
+function solution(meeting) {
+  let answer = 0;
+  meeting.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    else return a[1] - b[1];
+  });
+  let endTime = 0;
+  meeting.forEach((v) => {
+    if (v[0] >= endTime) {
+      answer++;
+      endTime = v[1]; // endTime 의 업데이트, For문에서 인덱스 + 1 역할과 같다.
+    }
+  });
+  return answer;
 }
