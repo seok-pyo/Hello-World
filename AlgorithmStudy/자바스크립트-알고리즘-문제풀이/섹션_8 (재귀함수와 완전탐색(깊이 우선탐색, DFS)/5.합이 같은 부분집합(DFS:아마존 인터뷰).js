8fb21b04e772e129ad770cc9174c10ffac80f6c7
SP-hong <hongseokpyou@gmail.com>
2023-09-09 17:24:45 +0900

    8-5. 합이 같은 부분집합, 강의 코드 추가. 문제 요구 사항 제대로 파악하는게 중요!

---
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_8 (재귀함수와 완전탐색(깊이 우선탐색, DFS)/5.합이 같은 부분집합(DFS:아마존 인터뷰).js
@@ -0,0 +1,46 @@
+// 문제 파악을 잘못한 케이스, 더했을 때 원소의 합이
+function solution(arr) {
+  let answer = "NO";
+  let n = arr.length - 1;
+  let result = [];
+  function DFS(v, a) {
+    if (v > n) {
+      return;
+    } else {
+      let set1 = a.slice();
+      let set2 = a.slice();
+      set1.push(arr[v]);
+      DFS(v + 1, set1);
+      result.push(set1);
+      DFS(v + 1, set2);
+    }
+  }
+  DFS(0, []);
+  let sum = result.map((v) => v.reduce((a, c) => a + c, 0));
+
+  for (let s of sum) if (sum.indexOf(s) !== sum.lastIndexOf(s)) answer = "YES";
+
+  return answer;
+}
+
+// 강의 코드 추가
+function solution(arr) {
+  let answer = "NO",
+    flag = 0;
+  let total = arr.reduce((a, c) => a + c, 0);
+  let n = arr.length;
+  function DFS(L, sum) {
+    if (flag) returnn;
+    if (L === n) {
+      if (total - sum === sum) {
+        answer = "YES";
+        flag = 1;
+      }
+    } else {
+      DFL(L + 1, sum + arr[L]);
+      DFL(L + 1, sum);
+    }
+  }
+  DFS(0, 0);
+  return answer;
+}
