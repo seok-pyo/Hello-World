d51aadf1e2b3806dc83de95a276fcd68267994af
SP-hong <hongseokpyou@gmail.com>
2023-09-11 17:28:24 +0900

    8-7.최대점수 구하기,마지막  조건의 위치 주의!

--- AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_8 (재귀함수와 완전탐색(깊이 우선탐색, DFS)/7.최대점수 구하기(DFS).js
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_8 (재귀함수와 완전탐색(깊이 우선탐색, DFS)/7.최대점수 구하기(DFS).js
@@ -1,12 +1,32 @@
 function solution(list, limit) {
   let answer = Number.MIN_SAFE_INTEGER;
-  let max = 0; // 자바스크립트는 스택에 마지막에 실행된 스택프레임의 값을 리턴한다.
+  //let max = 0; // 자바스크립트는 스택에 마지막에 실행된 스택프레임의 값을 리턴한다.
   let n = list.length;
   function DFS(L, sum, tsum) {
     if (L === n) {
       return;
     } else {
-      if (tsum <= limit) max = Math.max(answer, sum);
+      if (tsum <= limit) answer = Math.max(answer, sum);
+      DFS(L + 1, sum + list[L][0], tsum + list[L][1]);
+      DFS(L + 1, sum, tsum);
+    }
+  }
+
+  DFS(0, 0, 0);
+
+  return max;
+}
+
+// 강의 코드 추가
+
+function solution(list, limit) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let n = list.length;
+  function DFS(L, sum, tsum) {
+    if (tsum > limit) return;
+    if (L === n) {
+      answer = Math.max(answer, sum);
+    } else {
       DFS(L + 1, sum + list[L][0], tsum + list[L][1]);
       DFS(L + 1, sum, tsum);
     }
