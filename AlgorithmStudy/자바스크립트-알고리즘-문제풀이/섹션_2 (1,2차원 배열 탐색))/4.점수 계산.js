d7945983165b7639af21df24c285d77c97a54977
Son-Bin-O <hongseokpyou@gmail.com>
2023-09-16 01:43:03 +0900

    2-4. 점수계산, 다시 풀어보기

--- AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_2 (1,2차원 배열 탐색))/4.점수 계산.js
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_2 (1,2차원 배열 탐색))/4.점수 계산.js
@@ -11,3 +11,16 @@ function solution(score) {
   }
   return answer;
 }
+
+// 문제 다시 풀어보기
+function solution(arr) {
+  let answer = 0;
+  let flag = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // 인덱스로 접근할 필요가 없을 듯..
+    if (arr[i] === 1) flag++;
+    else flag = 0;
+    answer += arr[i] * flag;
+  }
+  return answer;
+}
