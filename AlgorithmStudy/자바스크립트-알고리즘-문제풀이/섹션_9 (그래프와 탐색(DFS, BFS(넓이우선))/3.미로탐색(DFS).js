2bb786e39aa6be3e23f418d8c99b18ceffc116a3
Son-Bin-O <hongseokpyou@gmail.com>
2023-09-17 15:25:24 +0900

    9-3.미로탐색(DFS)

---
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_9 (그래프와 탐색(DFS, BFS(넓이우선))/3.미로탐색(DFS).js
@@ -0,0 +1,24 @@
+function solution(board) {
+  answer = 0;
+  let n = board.length - 1; // borad의 index를 활용하기 떄문에 배열의 길이에서 -1을 해줘야 한다.
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+  function DFS(x, y) {
+    if (x === 6 && y === 6) {
+      answer++;
+    } else {
+      for (let i = 0; i < 4; i++) {
+        let nx = x + dx[i];
+        let ny = y + dy[i];
+        if (nx >= 0 && nx <= n && ny >= 0 && ny <= n && board[nx][ny] === 0) {
+          board[nx][ny] = 1;
+          DFS(nx, ny);
+          board[nx][ny] = 0;
+        }
+      }
+    }
+  }
+  board[0][0] = 1;
+  DFS(0, 0);
+  return answer;
+}
