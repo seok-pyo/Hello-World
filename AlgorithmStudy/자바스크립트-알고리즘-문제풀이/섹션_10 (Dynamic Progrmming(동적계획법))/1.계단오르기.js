c818838cb4bbdd8c0a7a764ffa6422c2db69aaf9
Son-Bin-O <hongseokpyou@gmail.com>
2023-09-19 15:14:12 +0900

    10-1. 계단오르기, 동적계획법

--- AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_10 (Dynamic Progrmming(동적계획법))/1.계단오르기.js
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_10 (Dynamic Progrmming(동적계획법))/1.계단오르기.js
@@ -12,3 +12,17 @@ function solution(n) {
   DFS(0, 0);
   return answer;
 }
+
+// 동적계획법이란 -> 점화식 처럼 문제의 조각을 가지고 전체의 답을 유추해내는 것.
+// dy 테이블이 있어야 한다.
+
+function solution(n) {
+  let answer = 0;
+  let dy = Array.from({ length: n + 1 }, () => 0);
+  dy[1] = 1;
+  dy[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    dy[i] = dy[i - 2] + dy[i - 1];
+  }
+  answer = dy[n];
+}
