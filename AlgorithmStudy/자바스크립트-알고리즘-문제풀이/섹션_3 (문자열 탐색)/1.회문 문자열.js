f1dea20cd6141eb29406353ffe5a700aceab4bd8
Son-Bin-O <hongseokpyou@gmail.com>
2023-09-20 16:13:48 +0900

    3-1. 회문 문자열

--- AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_3 (문자열 탐색)/1.회문 문자열.js
+++ AlgorithmStudy/자바스크립트-알고리즘-문제풀이/섹션_3 (문자열 탐색)/1.회문 문자열.js
@@ -7,3 +7,13 @@ function solution(t) {
     return "NO";
   }
 }
+
+function solution(s) {
+  let answer = "YES";
+  s = s.toLowerCase();
+  let len = s.length;
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (s[i] !== s[len - 1 - i]) return "NO";
+  }
+  return answer;
+}
