8561f29cff8a2de9162048453c9975ebc0ec95a3
seok-pyo <hongseokpyou@gmail.com>
2023-09-29 16:04:26 +0900

    튜플, 코드 시도

---
+++ AlgorithmStudy/Programmers/튜플.js
@@ -0,0 +1,15 @@
+// 배열 길이에 따라서 정렬하기
+// const result = [];
+// let tmp = Number.MAX_SAFE_INTEGER;
+// for (let x of arr) {
+//   tmp = Math.min(tmp, x.length);
+//   if (tmp < x.length) result.push(x);
+//   else result.unshift(x);
+// }
+
+// 중괄호 안에 있는 숫자 패턴을 추출하고 배열로 변환
+// let s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
+// let regex = /{([^}]+)}/g;
+// let matches = [...s.matchAll(regex)];
+
+// 배열 길이는 1부터 n까지
