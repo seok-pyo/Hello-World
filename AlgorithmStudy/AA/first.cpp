e86999217961bcc5fcc01a64d9ee3dd06d37a101
seok-pyo <hongseokpyou@gmail.com>
2023-11-25 23:46:53 +0900

    c: 정수형 변수와 그 크기 설명

---
+++ AlgorithmStudy/AA/first.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    // printf("@@@@@\n");
+    // printf("@   @\n");
+    // printf("@   @\n");
+    // printf("@@@@@\n");
+
+    int a, b=11, c=23;
+    a=7;
+
+    printf("%d\n", c);
+    return 0;
+}
