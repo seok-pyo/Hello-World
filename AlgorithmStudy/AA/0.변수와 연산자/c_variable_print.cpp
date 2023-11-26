05f0f7f2c559bc0e8e4c3851d3bbd9f1b6c6b457
seok-pyo <hongseokpyou@gmail.com>
2023-11-26 22:11:19 +0900

    scanf, 주소연산자

--- AlgorithmStudy/AA/0.변수와 연산자/c_variable_print.cpp
+++ AlgorithmStudy/AA/0.변수와 연산자/c_variable_print.cpp
@@ -2,9 +2,10 @@
 
 int main()
 {
-    int a, b, c, k;
-    a = b = c = 3;
-    k = a + 3 * 6 - 6 / c;
-    printf("혼합연산의 결과 = %d\n", k);
+    // scanf() 문
+    //  키보드로부터 값을 입력받아 변수에 저장할 때 사용
+    int a;
+    scanf("%d", &a); // a변수명 앞에 주소연산자 &를 꼭 붙인다!
+    printf("input value=%d\n", a);
     return 0;
 }
