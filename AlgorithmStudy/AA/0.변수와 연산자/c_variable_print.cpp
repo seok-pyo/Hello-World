d5c37958fbedf26dac0d3ca6fae1717ea5ed7e7b
seok-pyo <hongseokpyou@gmail.com>
2023-11-26 21:43:36 +0900

    문자를 숫자형 제어문자로 출력하면?

---
+++ AlgorithmStudy/AA/0.변수와 연산자/c_variable_print.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // int num;
+    // num = 3;
+    // printf("%d\n", num);
+    // return 0;
+    char str;
+    float a;
+    str = 'A';
+    a = 123456789;
+    printf("%d\n", str);
+    printf("%c\n", str);
+    printf("%f", a);
+    return 0;
+}
