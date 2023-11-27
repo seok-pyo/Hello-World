6d2e370cf6afab6469ec8d7ced4295620b6cde1c
seok-pyo <hongseokpyou@gmail.com>
2023-11-27 15:50:00 +0900

    반올림, math 헤더 없이 구현

--- AlgorithmStudy/AA/변수와 연산자/6_reverage.cpp
+++ AlgorithmStudy/AA/변수와 연산자/6_reverage.cpp
@@ -1,15 +1,20 @@
 #include <stdio.h>
-#include <math.h>
+// #include <math.h>
 
 int main()
 {
-    int a, b, c;
-    double result;
+    // int a, b, c;
+    // double result;
 
-    scanf("%d %d %d", &a, &b, &c);
+    // scanf("%d %d %d", &a, &b, &c);
 
-    result = (float)(a + b + c) / 3;
+    // result = (float)(a + b + c) / 3;
 
-    printf("%f\n", round(result * 100) / 100);
+    // printf("%f\n", round(result * 100) / 100);
+
+    double a;
+    scanf("%lf", &a);
+    a = (int)((a * 10) + 0.5);
+    printf("%f\n", a / 10);
     return 0;
 }
