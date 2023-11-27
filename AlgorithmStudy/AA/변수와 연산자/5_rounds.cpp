fddc0462803d0bddd7b8e2b2ac9f359ee7177ef1
seok-pyo <hongseokpyou@gmail.com>
2023-11-27 15:54:51 +0900

    반올림, 문제 파일 확인

--- AlgorithmStudy/AA/변수와 연산자/5_rounds.cpp
+++ AlgorithmStudy/AA/변수와 연산자/5_rounds.cpp
@@ -1,14 +1,21 @@
 #include <stdio.h>
-#include <math.h>
+// #include <math.h>
 
 int main()
 {
-    double a, result;
-
-    scanf("%lf", &a);
+    // double a, result;
 
     // scanf("%lf", &a);
-    printf("%f\n", round(a * 10) / 10);
+
+    // // scanf("%lf", &a);
+    // printf("%f\n", round(a * 10) / 10);
+
+    // 반올림 math 헤더 파일 없이 구현
+    double a;
+    scanf("%lf", &a);
+    a = (int)((a * 10) + 0.5);
+    printf("%f\n", a / 10);
+    return 0;
 
     return 0;
 }
--- AlgorithmStudy/AA/변수와 연산자/6_reverage.cpp
+++ AlgorithmStudy/AA/변수와 연산자/6_reverage.cpp
@@ -1,20 +1,14 @@
 #include <stdio.h>
-// #include <math.h>
+#include <math.h>
 
 int main()
 {
-    // int a, b, c;
-    // double result;
+    int a, b, c;
+    double result;
 
-    // scanf("%d %d %d", &a, &b, &c);
+    scanf("%d %d %d", &a, &b, &c);
 
-    // result = (float)(a + b + c) / 3;
+    result = (float)(a + b + c) / 3;
 
-    // printf("%f\n", round(result * 100) / 100);
-
-    double a;
-    scanf("%lf", &a);
-    a = (int)((a * 10) + 0.5);
-    printf("%f\n", a / 10);
-    return 0;
+    printf("%f\n", round(result * 100) / 100);
 }
