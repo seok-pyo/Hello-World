12f99e39051d0619b9cccc2c7d4068e4012140ae
seok-pyo <hongseokpyou@gmail.com>
2023-11-27 13:35:10 +0900

    실수 출력 시 반올림하는 방법

--- AlgorithmStudy/AA/변수와 연산자/c_scanf.cpp
+++ AlgorithmStudy/AA/변수와 연산자/c_scanf.cpp
@@ -3,10 +3,10 @@
 int main()
 
 {
-    // 두 수 입력받아서 출력, 두 수는 띄어쓰기로 구분해서 입력을 받는다.
-    int a, b;
-    printf("input two number ? ");
-    scanf("%d %d", &a, &b);
-    printf("input value=%d %d\n", a, b);
+    float a;
+    printf("input float number? ");
+    scanf("%f", &a);
+    printf("input value=%0.2f\n", a);
+    // %0.2f 는 실수를 출력할 때 소수점 3째자리에서 반올림하여 둘째 자리까지만 출력한다.
     return 0;
 }
