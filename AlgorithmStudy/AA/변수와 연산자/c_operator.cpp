5a52c6bd5f1682f129338ab76889e4697367a5b5
seok-pyo <hongseokpyou@gmail.com>
2023-11-27 13:46:33 +0900

    전위연산자, 후위연산자

--- AlgorithmStudy/AA/변수와 연산자/c_operator.cpp
+++ AlgorithmStudy/AA/변수와 연산자/c_operator.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-    // 1증가 연산자
     int a, b, k, m;
-    a = b = 2;
-    a++;
-    ++b;
-    printf("%d %d\n", a, b);
+    a = 3;
+    b = 2;
+    k = a--; // k는 3이 된다. 이 라인이 실행되면 a는 2가 된다
+    m = ++b;
+    printf("%d %d\n", k, m);
     return 0;
 }
