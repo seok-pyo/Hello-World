d15d63ca33d30494784118ce8f8c9df1f1b21087
seok-pyo <hongseokpyou@gmail.com>
2023-11-27 16:24:59 +0900

    연필의 개수, 소수점 활용 방법

--- AlgorithmStudy/AA/변수와 연산자/7_pencil.cpp
+++ AlgorithmStudy/AA/변수와 연산자/7_pencil.cpp
@@ -1,18 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    int a, result, remainder;
+    // int a, result, remainder;
 
-    scanf("%d", &a);
+    // scanf("%d", &a);
 
-    result = a / 12;
+    // result = a / 12;
 
-    remainder = a % 12;
+    // remainder = a % 12;
 
-    if (remainder > 0)
-        result = result + 1;
+    // if (remainder > 0)
+    //     result = result + 1;
 
-    printf("%d\n", result);
+    // printf("%d\n", result);
+
+    // 나머지가 있는 경우는 1다스가 더 필요한 경우
+    int n;
+    double res;
+    scanf("%d", &n);
+    res = n / 12.0;
+    res = ceil(res);
+
+    printf("%d\n", (int)res);
     return 0;
 }
