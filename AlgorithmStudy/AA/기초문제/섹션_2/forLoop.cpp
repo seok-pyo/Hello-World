2ca2b4137e8c17b92e6959ef70bfb32de411e418
seok-pyo <hongseokpyou@gmail.com>
2023-11-28 15:40:25 +0900

    forLoop

---
+++ AlgorithmStudy/AA/기초문제/섹션_2/forLoop.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    // int i;
+    // for (i = 1; i <= 5; i++)
+    // {
+    //     printf("hello\n");
+    // }
+
+    // int i;
+    // for (i = 1; i <= 5; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    // int i, sum = 0;
+    // for (i = 1; i <= 10; i++)
+    // {
+    //     if (i % 2 == 0)
+    //         printf("%d ", i);
+    // }
+
+    int i, sum = 0;
+    for (i = 1; i <= 10; i++)
+    {
+        if (i >= 3 && i < 6)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
