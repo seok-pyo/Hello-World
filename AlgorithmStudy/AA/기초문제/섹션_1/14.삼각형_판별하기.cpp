f7540317dd46b5a77aceb62fee41626db65f2b02
seok-pyo <hongseokpyou@gmail.com>
2023-11-28 15:58:41 +0900

    change directory

--- AlgorithmStudy/AA/기초문제/섹션_1/14.삼각형_판별하기.cpp
+++ AlgorithmStudy/AA/기초문제/섹션_1/14.삼각형_판별하기.cpp
@@ -9,8 +9,9 @@ int main()
     max = max > c ? max : c;
 
     if (a + b + c - max > max)
-        printf("%s\n", "YES");
-    // 형식 지정자는 변수나 동저으로 생성된 문자열을 출력할 때 사용한다.
+        printf("YES");
+    // printf("%s\n", "YES");
+    // 형식 지정자는 변수나 동적으로 생성된 문자열을 출력할 때 사용한다.
     else
         printf("%s\n", "NO");
     return 0;
