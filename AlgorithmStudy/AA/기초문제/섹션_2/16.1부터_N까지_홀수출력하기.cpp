2f4674b26e52ed4d7e647105692f75f29fa176e0
seok-pyo <hongseokpyou@gmail.com>
2023-11-30 00:02:44 +0900

    홀수 출력 코드 추가

--- AlgorithmStudy/AA/기초문제/섹션_2/16.1부터_N까지_홀수출력하기.cpp
+++ AlgorithmStudy/AA/기초문제/섹션_2/16.1부터_N까지_홀수출력하기.cpp
@@ -5,10 +5,16 @@ int main()
     int n, i;
     scanf("%d", &n);
 
-    for (i = 1; i <= n; i++)
+    // for (i = 1; i <= n; i++)
+    // {
+    //     if (i % 2 != 0)
+    //         printf("%d ", i);
+    // }
+
+    // if문을 사용하지 않는다면
+    for (i = 1; i <= n; i = i + 2)
     {
-        if (i % 2 != 0)
-            printf("%d ", i);
+        printf("%d ", i);
     }
 
     return 0;
