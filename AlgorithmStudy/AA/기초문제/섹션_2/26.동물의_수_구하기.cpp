328610041a451ce3ba27b890bc218a3360ad690d
seok-pyo <hongseokpyou@gmail.com>
2023-11-30 13:32:50 +0900

    연립방정식 반복문으로 풀이

--- AlgorithmStudy/AA/기초문제/섹션_2/26.동물의_수_구하기.cpp
+++ AlgorithmStudy/AA/기초문제/섹션_2/26.동물의_수_구하기.cpp
@@ -2,12 +2,28 @@
 
 int main()
 {
-    int sum, leg, c, r;
+    // 연립방정식 풀이
+    // int sum, leg, c, r;
+    // scanf("%d %d", &sum, &leg);
+
+    // r = (leg - 2 * sum) / 2;
+    // c = sum - r;
+
+    // printf("%d %d\n", r, c);
+
+    // 연립방정식, 반복문 풀이
+    int leg, sum, i;
+
     scanf("%d %d", &sum, &leg);
 
-    r = (leg - 2 * sum) / 2;
-    c = sum - r;
+    for (i = 1; i < sum; i++)
+    {
+        if ((4 * i) + 2 * (sum - i) == leg)
+        {
+            printf("%d %d\n", i, sum - i);
+            break;
+        }
+    }
 
-    printf("%d %d\n", r, c);
     return 0;
 }
