5e6fe47b3c80a6c26c5f9ce4a18f1c44a1bf3f4e
seok-pyo <hongseokpyou@gmail.com>
2023-11-30 14:28:45 +0900

    두 자리 숫자 만들기

---
+++ AlgorithmStudy/AA/기초문제/섹션_3/34.두_자리_숫자_만들기.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, i, j;
+    scanf("%d", &a);
+
+    for (i = 1; i <= a; i++)
+    {
+        for (j = 1; j <= a; j++)
+        {
+            if (i != j)
+            {
+                printf("%d%d", i, j);
+                printf("\n");
+            }
+        }
+    }
+
+    return 0;
+}
