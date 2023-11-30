108b63c04e25e1fd087f77738bea57f736a363a0
seok-pyo <hongseokpyou@gmail.com>
2023-11-30 15:45:48 +0900

    숫자로 만들어서 판별

--- AlgorithmStudy/AA/기초문제/섹션_3/35.특정_두자리_수_만들기.cpp
+++ AlgorithmStudy/AA/기초문제/섹션_3/35.특정_두자리_수_만들기.cpp
@@ -2,29 +2,44 @@
 
 int main()
 {
-    int a, i, j, cnt = 0, limit, one;
-    scanf("%d", &a);
+    // int a, i, j, cnt = 0, limit, one;
+    // scanf("%d", &a);
 
-    limit = (int)a / 10;
-    one = a % 10;
+    // limit = (int)a / 10;
+    // one = a % 10;
 
-    for (i = 2; i <= limit; i++)
+    // for (i = 2; i <= limit; i++)
+    // {
+    //     for (j = 1; j <= limit - 1; j++)
+    //     {
+    //         if (i == limit && j <= one)
+    //         {
+    //             cnt += 1;
+    //             printf("%d%d\n", i, j);
+    //         }
+    //         if (i != limit && i > j)
+    //         {
+    //             cnt += 1;
+    //             printf("%d%d\n", i, j);
+    //         }
+    //     }
+    // }
+    // printf("%d\n", cnt);
+
+    int c, i, j, cnt = 0, tmp;
+    scanf("%d", &c);
+    for (i = 1; i <= 9; i++)
     {
-        for (j = 1; j <= limit - 1; j++)
+        for (j = 1; j < i; j++)
         {
-            if (i == limit && j <= one)
+            tmp = 1 * 10 + j;
+            if (tmp <= c)
             {
-                cnt += 1;
-                printf("%d%d\n", i, j);
-            }
-            if (i != limit && i > j)
-            {
-                cnt += 1;
-                printf("%d%d\n", i, j);
+                printf("%d\n", tmp);
+                cnt++;
             }
         }
     }
     printf("%d\n", cnt);
-
     return 0;
 }
