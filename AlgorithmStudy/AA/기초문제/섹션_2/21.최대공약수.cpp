cc8542bb490e5b5cf876073ca71b844231a0c159
seok-pyo <hongseokpyou@gmail.com>
2023-11-30 13:23:39 +0900

    최대공약수_강의풀이

--- AlgorithmStudy/AA/기초문제/섹션_2/21.최대공약수.cpp
+++ AlgorithmStudy/AA/기초문제/섹션_2/21.최대공약수.cpp
@@ -2,24 +2,37 @@
 
 int main()
 {
-    int a, b, remain;
+    // 유클리드 호제법
+    // int a, b, remain;
+    // scanf("%d %d", &a, &b);
+
+    // if (a < b)
+    // {
+    //     remain = b % a;
+    // }
+
+    // while (remain)
+    // {
+    //     b = a;
+    //     a = remain;
+    //     remain = b % a;
+    // }
+
+    // if (remain == 0)
+    // {
+    //     printf("%d\n", a);
+    // }
+
+    int a, b, min, i = 1;
     scanf("%d %d", &a, &b);
 
-    if (a < b)
-    {
-        remain = b % a;
-    }
+    min = a < b ? a : b;
 
-    while (remain)
+    for (i = min; i >= 1; i--)
     {
-        b = a;
-        a = remain;
-        remain = b % a;
-    }
-
-    if (remain == 0)
-    {
-        printf("%d\n", a);
+        if (a % i == 0 && b % i == 0)
+            printf("%d\n", i);
+        break;
     }
 
     return 0;
