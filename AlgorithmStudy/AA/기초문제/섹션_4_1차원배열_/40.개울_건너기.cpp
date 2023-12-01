75fa48cf75168c700f8ad55c782e4591e1c83b28
seok-pyo <hongseokpyou@gmail.com>
2023-12-01 23:15:21 +0900

    개울_건너기, 수학적 사고

--- AlgorithmStudy/AA/기초문제/섹션_4_1차원배열_/40.개울_건너기.cpp
+++ AlgorithmStudy/AA/기초문제/섹션_4_1차원배열_/40.개울_건너기.cpp
@@ -7,13 +7,13 @@ int main()
     int n, i;
     scanf("%d", &n);
     arr[0] = 1;
-    arr[1] = 1;
+    arr[1] = 2;
 
     for (i = 2; i <= n + 1; i++)
     {
         arr[i] = arr[i - 2] + arr[i - 1];
     }
 
-    printf("%d\n", arr[n + 1]);
+    printf("%d\n", arr[n]);
     return 0;
 }
