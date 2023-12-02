d99cc945128b7cb4eabfc1daa20b61bbd9c01563
seok-pyo <hongseokpyou@gmail.com>
2023-12-02 11:06:37 +0900

    2차원 배열 출력 3

---
+++ AlgorithmStudy/AA/섹션_5_2차원배열/44.2차원_배열_출력_3.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int arr[11][11];
+int main()
+{
+    int n, i, j, cnt = 1;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        arr[i][i] = 2 * i + 1;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i][n - i - 1] != 0)
+            continue;
+        arr[i][n - i - 1] = 2 * cnt;
+        cnt += 1;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%3d", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
