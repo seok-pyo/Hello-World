199d7ee60c549cac0feca5aefad72e0e10a3d128
seok-pyo <hongseokpyou@gmail.com>
2023-12-02 12:40:40 +0900

    2차원 배열 출력-3 강의풀이

--- AlgorithmStudy/AA/섹션_5_2차원배열/44.2차원_배열_출력_3.cpp
+++ AlgorithmStudy/AA/섹션_5_2차원배열/44.2차원_배열_출력_3.cpp
@@ -5,27 +5,49 @@ int main()
 {
     int n, i, j, cnt = 1;
 
-    scanf("%d", &n);
+    // scanf("%d", &n);
 
+    // for (i = 0; i < n; i++)
+    // {
+    //     for (j = 0; j < n; j++)
+    //     {
+    //         arr[i][j] = 0;
+    //     }
+    // }
+
+    // for (i = 0; i < n; i++)
+    // {
+    //     arr[i][i] = 2 * i + 1;
+    // }
+
+    // for (i = 0; i < n; i++)
+    // {
+    //     if (arr[i][n - i - 1] != 0)
+    //         continue;
+    //     arr[i][n - i - 1] = 2 * cnt;
+    //     cnt += 1;
+    // }
+
+    // for (i = 0; i < n; i++)
+    // {
+    //     for (j = 0; j < n; j++)
+    //     {
+    //         printf("%3d", arr[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    scanf("%d", &n);
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < n; j++)
         {
-            arr[i][j] = 0;
+            if (i == j || i + j == n - 1)
+            {
+                arr[i][j] = cnt;
+                cnt += 1;
+            }
         }
-    }
-
-    for (i = 0; i < n; i++)
-    {
-        arr[i][i] = 2 * i + 1;
-    }
-
-    for (i = 0; i < n; i++)
-    {
-        if (arr[i][n - i - 1] != 0)
-            continue;
-        arr[i][n - i - 1] = 2 * cnt;
-        cnt += 1;
     }
 
     for (i = 0; i < n; i++)
