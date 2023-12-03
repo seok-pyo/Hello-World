6e62b05256bb18a4b3fde9aa206f4bdb6c829be0
seok-pyo <hongseokpyou@gmail.com>
2023-12-03 17:15:09 +0900

    나이계산

--- AlgorithmStudy/algorithm_cpp/1_코드 구현/5.나이계산.cpp
+++ AlgorithmStudy/algorithm_cpp/1_코드 구현/5.나이계산.cpp
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main()
+{
+    char a[20];
+    int year, age;
+    scanf("%s", a);
+    if (a[7] == '1' || a[7] == '2')
+        year = 1900 + ((a[0] - 48) * 10 + (a[1] - 48));
+    else
+        year = 2000 + ((a[0] - 48) * 10 + (a[1] - 48));
+    age = 2023 - year + 1;
+    printf("%d\n", age);
+
+    return 0;
+}
