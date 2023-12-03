e4b1e3b455e7a8e1aaa8e8a539a806853b06f13f
seok-pyo <hongseokpyou@gmail.com>
2023-12-03 16:43:40 +0900

    M의 배수합

--- AlgorithmStudy/algorithm_cpp/1_코드 구현/1.1부터_N까지의_M_배수합.cpp
+++ AlgorithmStudy/algorithm_cpp/1_코드 구현/1.1부터_N까지의_M_배수합.cpp
@@ -2,9 +2,15 @@
 
 int main()
 {
-    int n, m;
+    int n, m, i, sum = 0;
     scanf("%d %d", &n, &m);
 
-    printf("%d\n", n * m);
+    for (i = 1; i <= n; i++)
+    {
+        if (i % m == 0)
+            sum += i;
+    }
+
+    printf("%d\n", sum);
     return 0;
 }
