9d5954f2315b9dbf9bbac4487ac0f41ca5c5786c
seok-pyo <hongseokpyou@gmail.com>
2023-12-03 22:33:50 +0900

    모두의 약수

--- AlgorithmStudy/algorithm_cpp/1_코드 구현/9.모두의_약수.cpp
+++ AlgorithmStudy/algorithm_cpp/1_코드 구현/9.모두의_약수.cpp
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
-int Prime(int a)
-{
-    int i, cnt = 0;
-    for (i = 1; i <= a; i++)
-    {
-        if (a % i == 0)
-            cnt += 1;
-    }
-
-    return cnt;
-}
-
+int a[50001];
 int main()
 {
-    int n, i;
-
+    int n, i, j;
     scanf("%d", &n);
+
     for (i = 1; i <= n; i++)
     {
-        printf("%d ", Prime(i));
+        for (j = i; j <= n; j = j + i)
+        {
+            a[j] += 1;
+        }
+    }
+
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d ", a[i]);
     }
 
     return 0;
