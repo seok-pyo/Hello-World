cc62974e20dde56043d82771ae8f7de3cfe2a4fd
seok-pyo <hongseokpyou@gmail.com>
2023-12-05 08:08:04 +0900

    뒤집은 소수-숫자만 추출 참고(4A)

---
+++ AlgorithmStudy/algorithm_cpp/1_코드 구현/14.뒤집은_소수.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int reverse(int x)
+{
+    int moc, na, sum = 0;
+    moc = x;
+    while (moc > 0)
+    {
+        na = moc % 10;
+        moc = moc / 10;
+        sum = (sum * 10) + na;
+    }
+    return sum;
+};
+
+bool isPrime(int x)
+{
+    int i;
+    for (i = 2; i < x; i++)
+    {
+        if (x % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int n, i, num, tmp;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        tmp = reverse(num);
+        if (isPrime(tmp))
+            printf("%d ", tmp);
+    }
+    return 0;
+}
--- AlgorithmStudy/algorithm_cpp/1_코드 구현/tempCodeRunnerFile.cpp
+++ AlgorithmStudy/algorithm_cpp/1_코드 구현/tempCodeRunnerFile.cpp
@@ -1,1 +1,13 @@
-char a[n + 1];
+int main()
+{
+    int n, i, num, tmp;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        tmp = reverse(num);
+        if (isPrime(tmp))
+            printf("%d ", tmp);
+    }
+    return 0;
+}
