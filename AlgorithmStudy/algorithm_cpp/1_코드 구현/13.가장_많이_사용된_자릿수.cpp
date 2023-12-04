ac5034e99ecb6b389609d4c589aaa70de97fee12
seok-pyo <hongseokpyou@gmail.com>
2023-12-05 08:24:50 +0900

    가장 많이 사용된 자릿수_강의코드

--- AlgorithmStudy/algorithm_cpp/1_코드 구현/13.가장_많이_사용된_자릿수.cpp
+++ AlgorithmStudy/algorithm_cpp/1_코드 구현/13.가장_많이_사용된_자릿수.cpp
@@ -3,16 +3,23 @@
 int a[11];
 int main()
 {
-    int n, moc, na, res, i, tmp = -2147000000;
-    scanf("%d", &n);
+    // int n, moc, na, res, i, tmp = -2147000000;
+    int n, digit, res, i, tmp = -2147000000;
+    char s[101];
+    scanf("%s", s);
 
-    moc = n;
-    while (moc > 0)
+    for (i = 0; s[i] != '\0'; i++)
     {
-        na = moc % 10;
-        moc = moc / 10;
-        a[na] += 1;
+        digit = s[i] - 48;
+        a[digit] += 1;
     }
+    // moc = n;
+    // while (moc > 0)
+    // {
+    //     na = moc % 10;
+    //     moc = moc / 10;
+    //     a[na] += 1;
+    // }
 
     for (i = 0; i < 10; i++)
     {
