4c401bf8f238323c801c4523a53d9ef0f68c1223
seok-pyo <hongseokpyou@gmail.com>
2023-12-09 11:31:30 +0900

    팩토리얼 표현법 시도

---
+++ AlgorithmStudy/algorithm_cpp/1_코드 구현/27.N팩토리얼의_표현법.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+long long fac(int x)
+{
+    if (x <= 1)
+        return 1;
+    else
+        return x * fac(x - 1);
+}
+
+long long res[50000];
+int main()
+{
+    int n, p = 2, idx = 0, i;
+    scanf("%d", &n);
+    long long f = fac(n);
+    while (f > 1)
+    {
+        while (f % p != 0)
+        {
+            if (p == 2)
+            {
+                p += 1;
+                idx += 1;
+            }
+            else
+            {
+                p += 2;
+                idx += 1;
+            }
+        }
+        f = f / p;
+        res[idx] += 1;
+    }
+    for (i = 0; i <= idx; i++)
+    {
+        printf("%lld ", res[i]);
+    }
+    return 0;
+}
