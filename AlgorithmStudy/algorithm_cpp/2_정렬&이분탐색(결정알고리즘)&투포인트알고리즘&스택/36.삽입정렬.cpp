c9d41a3f29cf12bc693d905d8362213e8c0824e8
seok-pyo <hongseokpyou@gmail.com>
2023-12-12 12:18:47 +0900

    삽입정렬

---
+++ AlgorithmStudy/algorithm_cpp/2_정렬&이분탐색(결정알고리즘)&투포인트알고리즘&스택/36.삽입정렬.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j, tmp;
+    int a[101];
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 1; i < n; i++)
+    {
+        tmp = a[i];
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (a[j] > tmp)
+            {
+                a[j + 1] = a[j];
+            }
+            else
+                break;
+        }
+        a[j + 1] = tmp;
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
