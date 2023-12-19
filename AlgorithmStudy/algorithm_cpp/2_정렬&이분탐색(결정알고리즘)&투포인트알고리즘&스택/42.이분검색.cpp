8e1aa22a60a6f5a4a45f29765d025a07234692c7
seok-pyo <hongseokpyou@gmail.com>
2023-12-19 12:05:19 +0900

    이분검색_강의 코드

--- AlgorithmStudy/algorithm_cpp/2_정렬&이분탐색(결정알고리즘)&투포인트알고리즘&스택/42.이분검색.cpp
+++ AlgorithmStudy/algorithm_cpp/2_정렬&이분탐색(결정알고리즘)&투포인트알고리즘&스택/42.이분검색.cpp
@@ -8,28 +8,31 @@ int main()
 {
     int n, m, s, e, i, res, tmp;
     scanf("%d %d", &n, &m);
-    vector<int> a(n + 1);
-    for (i = 1; i <= n; i++)
+    vector<int> a(n);
+    for (i = 0; i < n; i++)
     {
         scanf("%d", &a[i]);
     }
     sort(a.begin(), a.end());
 
-    s = 1;
-    e = n;
-    tmp = s + e / 2;
-    while (m != a[tmp])
+    s = 0;
+    e = n - 1;
+    while (s <= e)
     {
-        tmp = s + e / 2;
-        if (a[tmp] > m)
+        tmp = (s + e) / 2;
+        if (a[tmp] == m)
+        {
+            printf("%d ", tmp + 1);
+            return 0;
+        }
+        else if (a[tmp] > m)
         {
             e = tmp - 1;
         }
-        else if (a[tmp] < m)
+        else
         {
             s = tmp + 1;
         }
     }
-    printf("%d ", tmp);
     return 0;
 }
