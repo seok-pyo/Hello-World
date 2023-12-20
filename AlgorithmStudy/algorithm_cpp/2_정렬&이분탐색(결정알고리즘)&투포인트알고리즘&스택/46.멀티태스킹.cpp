354920ecf4098c58b711def30cfefcb72b3b1d49
seok-pyo <hongseokpyou@gmail.com>
2023-12-20 15:27:57 +0900

    멀티태스킹 강의 코드 추가(요구사항 파악 연습)

--- AlgorithmStudy/algorithm_cpp/2_정렬&이분탐색(결정알고리즘)&투포인트알고리즘&스택/46.멀티태스킹.cpp
+++ AlgorithmStudy/algorithm_cpp/2_정렬&이분탐색(결정알고리즘)&투포인트알고리즘&스택/46.멀티태스킹.cpp
@@ -3,35 +3,45 @@
 using namespace std;
 int main()
 {
-    int i, n, k, time, pos = 0;
+    int i, n, k, time, pos = 0, tot = 0;
     scanf("%d", &n);
     vector<int> w(n + 1);
     for (i = 1; i <= n; i++)
     {
         scanf("%d", &w[i]);
+        tot += w[i];
     }
     scanf("%d", &k);
     time = k;
+    // 전체 작업 시간과 정전시간이 시작하는 시간이 같으면 정전 후
+    // 모든 작업 완료이기 때문에 -1 리턴
+    if (k >= tot)
+    {
+        printf("-1\n");
+        return 0;
+    }
     while (time)
     {
         pos++; // pos, 인덱스는 계속 이동해야 한다.
         if (pos > n)
         {
-            pos = 0;
+            pos = 1;
         }
         if (w[pos] != 0)
         {
             time--;
-            w[pos] = w[pos] - 1;
+            w[pos]--;
         }
     }
 
-    for (i = 1; i <= n; i++)
+    while (true)
     {
-        if (w[i] != 0)
-        {
-            printf("%d ", i);
-        }
+        pos++;
+        if (pos > n)
+            pos = 1;
+        if (w[pos] != 0)
+            break;
     }
+    printf("%d\n", pos);
     return 0;
 }
