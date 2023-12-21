e35eafce5390921c1592d2ac0d8673c8caac6a5e
seok-pyo <hongseokpyou@gmail.com>
2023-12-21 17:47:04 +0900

    봉우리, 강의 코드 추가

--- AlgorithmStudy/algorithm_cpp/2_정렬&이분탐색(결정알고리즘)&투포인트알고리즘&스택/47.봉우리.cpp
+++ AlgorithmStudy/algorithm_cpp/2_정렬&이분탐색(결정알고리즘)&투포인트알고리즘&스택/47.봉우리.cpp
@@ -1,10 +1,18 @@
 #include <stdio.h>
-
-int a[51][51];
+#include <vector>
+// int a[51][51]; > 주위에 0으로 둘러야 하므로 양쪽을 계산해서 52개의 공간이 필요하다.
+using namespace std;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
 int main()
 {
-    int i, j, cnt = 0, cur, n;
+    freopen("input.txt", "rt", stdin);
+    int i, j, k, cnt = 0, cur, n, flag;
     scanf("%d", &n);
+    vector<vector<int> // vector로 2중 배열 생성하기
+
+           >
+        a(n + 2, vector<int>(n + 2, 0));
     for (i = 1; i <= n; i++)
     {
         for (j = 1; j <= n; j++)
@@ -13,18 +21,35 @@ int main()
         }
     }
 
-    for (i = 1; i <= n; i++)
+    // for (i = 1; i <= n; i++)
+    // {
+    //     for (j = 1; j <= n; j++)
+    //     {
+    //         cur = a[i][j];
+    //         if (cur > a[i][j - 1] &&
+    //             cur > a[i][j + 1] &&
+    //             cur > a[i - 1][j] &&
+    //             cur > a[i + 1][j])
+    //         {
+    //             cnt++;
+    //         }
+    //     }
+    // }
+    for (i = 1; i <= n; i++) // flag와 dy, dy 배열로 k 반복문 활용하기
     {
         for (j = 1; j <= n; j++)
         {
-            cur = a[i][j];
-            if (cur > a[i][j - 1] &&
-                cur > a[i][j + 1] &&
-                cur > a[i - 1][j] &&
-                cur > a[i + 1][j])
+            flag = 0;
+            for (k = 0; k < 4; k++)
             {
-                cnt++;
+                if (a[i + dx[k]][j + dy[k]] >= a[i][j])
+                {
+                    flag = 1;
+                    break;
+                }
             }
+            if (flag == 0)
+                cnt++;
         }
     }
     printf("%d\n", cnt);
