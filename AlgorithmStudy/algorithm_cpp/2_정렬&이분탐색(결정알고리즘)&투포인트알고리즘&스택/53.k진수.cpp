45344bd96f722075203468c4434fdf08930e99f5
seok-pyo <hongseokpyou@gmail.com>
2023-12-23 14:54:59 +0900

    k진수 강의 코드

---
+++ AlgorithmStudy/algorithm_cpp/2_정렬&이분탐색(결정알고리즘)&투포인트알고리즘&스택/53.k진수.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int stack[100], top = -1; // stack의 인덱스는 -1로 초기화
+
+void push(int x)
+{
+    stack[++top] = x; // 전위연산자 사용 top의 위치를 먼저 바꾸고 top가 가르키는 곳에 저장
+}
+int pop()
+{
+    return stack[top--]; // 후위연산자로 현재 top의 값을 리턴하고, top를 그 이전에 값을 가르키도록 수정
+}
+int main()
+{
+    freopen("input.txt", "rt", stdin);
+    int n, k;
+    char str[20] = "0123456789ABCDEFG"; // k진수 변환을 위한 문자표
+    scanf("%d %d", &n, &k);
+    while (n > 0) // 몫이 0이 될 때까지 나머지를 취한다.
+    {
+        push(n % k);
+        n = n / k;
+    }
+    while (top != -1) // 전체 스택을 출력
+    {
+        printf("%c", str[pop()]);
+    }
+    printf("\n");
+    return 0;
+}
