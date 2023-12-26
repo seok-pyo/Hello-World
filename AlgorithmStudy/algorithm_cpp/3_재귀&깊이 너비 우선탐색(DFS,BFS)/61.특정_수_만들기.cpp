b313ecc9adad04b98dee7550f2f96dccdc6f25b5
seok-pyo <hongseokpyou@gmail.com>
2023-12-26 18:48:10 +0900

    특정 수 만들기, path 트리 그려보기

--- AlgorithmStudy/algorithm_cpp/3_재귀&깊이 너비 우선탐색(DFS,BFS)/61.특정_수_만들기.cpp
+++ AlgorithmStudy/algorithm_cpp/3_재귀&깊이 너비 우선탐색(DFS,BFS)/61.특정_수_만들기.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int n, m, cnt = 0;
-int a[11];
+int a[11], path[11];
 void DFS(int L, int sum)
 {
     if (L == n + 1)
@@ -9,13 +9,21 @@ void DFS(int L, int sum)
         if (sum == m)
         {
             cnt++;
+            for (int i = 1; i < L; i++)
+            { // L은 하나 증가한 상태로 넘어옴으로 i는 L까지 진행
+                printf("%d ", path[i]);
+            }
+            puts("");
         }
     }
     else
     {
+        path[L] = a[L];
         DFS(L + 1, sum + a[L]);
-        DFS(L + 1, sum);
+        path[L] = -a[L];
         DFS(L + 1, sum - a[L]);
+        path[L] = 0;
+        DFS(L + 1, sum);
     }
 }
 int main()
