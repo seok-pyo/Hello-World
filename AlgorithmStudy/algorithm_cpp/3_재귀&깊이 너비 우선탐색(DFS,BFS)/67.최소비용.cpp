7d8ff4143946ef69579faaf7a46ec12e1430bc59
seok-pyo <hongseokpyou@gmail.com>
2023-12-29 15:30:08 +0900

    최소비용 강의코드

--- AlgorithmStudy/algorithm_cpp/3_재귀&깊이 너비 우선탐색(DFS,BFS)/67.최소비용.cpp
+++ AlgorithmStudy/algorithm_cpp/3_재귀&깊이 너비 우선탐색(DFS,BFS)/67.최소비용.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int map[30][30], ch[30], min = 2147000000, n, sum=0;
-void DFS(int v){
+int map[30][30], ch[30], min = 2147000000, n;
+void DFS(int v, int sum){
     int i;
     if(v == n){
         min = min > sum ? sum : min;
@@ -8,10 +8,9 @@ void DFS(int v){
         for(i=1; i<=n; i++){
             if(map[v][i]!=0 && ch[i] == 0){
                 ch[i] = 1;
-                sum += map[v][i];
-                DFS(i);
+                DFS(i, sum+map[v][i]); // 덧셈할당연산자를 사용하게 되면, sum이 for 
+                // 반복문 안에서 계속 누적되어서 DFS를 실행했을 때 원하는 답을 구할 수 없다.
                 ch[i] = 0;
-                sum -= map[v][i];
             }
         }
     }
@@ -25,7 +24,7 @@ int main(){
         map[a][b] = c;
     }
     ch[1] = 1;
-    DFS(1);
+    DFS(1, 0);
     printf("%d\n", min);
     return 0;
 }
