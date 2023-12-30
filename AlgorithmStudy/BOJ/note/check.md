5e175919d6bc7ac7c57eac0a6bc7a040ffe6f4ba
seok-pyo <hongseokpyou@gmail.com>
2023-12-30 18:35:28 +0900

    check

---
+++ AlgorithmStudy/BOJ/note/check.md
@@ -0,0 +1,1 @@
+2023-12-30 : 조건문 - 세 개의 주사위 https://www.acmicpc.net/problem/2480
--- AlgorithmStudy/BOJ/note/test.cpp
+++ AlgorithmStudy/BOJ/note/test.cpp
@@ -1,26 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    int a, b, c, res, max, s;
-    scanf("%d %d %d", &a, &b, &c);
-    if(a==b&&b==c){
-        res = 10000 + (a*1000);
-    } else if(a!=b && a!=c && b!=c) {
-        max = a < b ? b : a;
-        max = max < c ? c : max;
-        res = max * 100;
+    int x;
+    scanf("%d", &x);
+    if(x%4==0 && x%100!=0) {
+        printf("%d\n", 1);
+    } else if(x%400==0){
+        printf("%d\n", 1);
     } else {
-        if(a==b){
-            s = a;
-            res = 1000 + (s*100);
-        } else if(a==c){
-            s = a;
-            res = 1000 + (s*100);
-        } else {
-            s = b;
-            res = 1000 + (s*100);
-        }
+        printf("%d\n", 0);
     }
-    printf("%d\n", res);
     return 0;
 }
