0ee11e99a44299d68b5635da112afe7df4cbb0bc
seok-pyo <hongseokpyou@gmail.com>
2023-12-30 18:19:35 +0900

    주사위 세 개

--- AlgorithmStudy/BOJ/note/test.cpp
+++ AlgorithmStudy/BOJ/note/test.cpp
@@ -1,39 +1,26 @@
 #include <stdio.h>
-#include <vector>
-using namespace std;
-// int main(){
-//     printf("|\\_/|\n");
-//     printf("|q p|   /}\n");
-//     printf("( 0 )\"\"\"\\\n");
-//     printf("|\"^\"`    |\n");
-//     printf("||_/=\\\\__|\n");
-//     return 0;
-// }
 
 int main(){
-    int a, b, i;
-    vector<int> ch;
-    while(1){
-        scanf("%d %d", &a, &b);
-        if(a==0 && b==0) break;
-        if(b%a==0){
-            ch.push_back(1);
-        } else if(a%b==0){
-            ch.push_back(2);
+    int a, b, c, res, max, s;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a==b&&b==c){
+        res = 10000 + (a*1000);
+    } else if(a!=b && a!=c && b!=c) {
+        max = a < b ? b : a;
+        max = max < c ? c : max;
+        res = max * 100;
+    } else {
+        if(a==b){
+            s = a;
+            res = 1000 + (s*100);
+        } else if(a==c){
+            s = a;
+            res = 1000 + (s*100);
         } else {
-             ch.push_back(3);
-           
+            s = b;
+            res = 1000 + (s*100);
         }
     }
-
-    for(i=0; i<ch.size(); i++){
-        if(ch[i]==1){
-            printf("factor\n");
-        } else if(ch[i]==2) {
-            printf("multiple\n");
-        } else if(ch[i]==3) {
-            printf("neither\n");
-        }
-    }
+    printf("%d\n", res);
     return 0;
 }
