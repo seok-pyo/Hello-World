c0fc0ca868ebf577e305739f602c8565d1f995a0
seok-pyo <hongseokpyou@gmail.com>
2024-01-02 22:34:59 +0900

    queue 활용

--- AlgorithmStudy/BOJ/note/test.cpp
+++ AlgorithmStudy/BOJ/note/test.cpp
@@ -1,54 +1,54 @@
-#include <stdio.h>
-#include <algorithm>
-#include <stack>
+#include <iostream>
+#include <queue>
 #include <vector>
+#include <string>
 using namespace std;
 int main(){
-    stack<int> s;
+    queue<int> q;
     vector<int> res;
-    int n, i, a, p;
+    int n, i, p, num;
+    string a;
     scanf("%d", &n);
     for(i=0; i<n; i++){
-        scanf("%d", &a);
-        if(a==1){
-            scanf("%d", &p);
-            s.push(p);
-        } 
-        if(a==2){
-            if(!s.empty()) {
-                res.push_back(s.top());
-                //printf("%d\n", s.top());
-                s.pop();
-            } else {
+        cin >> a;
+        if(a=="push"){
+            scanf("%d", &num);
+            q.push(num);
+        }
+        if(a=="pop"){
+            if(q.empty()){
                 res.push_back(-1);
-                //printf("-1\n");
+            } else {
+                res.push_back(q.front());
+                q.pop();
             }
-        } 
-        if(a==3){
-            //printf("%lu\n", s.size());
-            res.push_back(s.size());
-        } 
-        if(a==4){
-            if(s.empty()) {
-                // printf("%d\n", 1);
+        }
+        if(a=="size"){
+            res.push_back(q.size());
+        }
+        if(a=="empty"){
+            if(q.empty()){
                 res.push_back(1);
             } else {
                 res.push_back(0);
-                // printf("%d\n", 0);
-            }
-        } 
-        if(a==5){
-            if(!s.empty()) {
-                res.push_back(s.top());
-                //printf("%d\n", s.top());
-            }
-            else {
-                res.push_back(-1);
-                //printf("%d\n", -1);
             }
         }
-
-    } 
+        if(a=="front"){
+            if(q.empty()){
+                res.push_back(-1);
+            } else {
+                res.push_back(q.front());
+            }
+        }
+        if(a=="back"){
+            if(q.empty()){
+                res.push_back(-1);
+            } else {
+                res.push_back(q.back());
+            }
+        }       
+    }
+    
     for(i=0; i<res.size(); i++){
         printf("%d\n", res[i]);
     }
