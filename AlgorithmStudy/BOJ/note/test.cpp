e4554cc7fed464752d3a0f5497306d3be88a5205
seok-pyo <hongseokpyou@gmail.com>
2024-01-02 22:07:14 +0900

    stack 활용하기

--- AlgorithmStudy/BOJ/note/test.cpp
+++ AlgorithmStudy/BOJ/note/test.cpp
@@ -1,16 +1,58 @@
 #include <stdio.h>
 #include <algorithm>
-#include <set>
+#include <stack>
+#include <vector>
 using namespace std;
-
 int main(){
-    int a, res, tmp, cnt = 0;   
-    set<int> unique;
-    for(int i = 0; i < 10; i++){
+    stack<int> s;
+    vector<int> res;
+    int n, i, a, p;
+    scanf("%d", &n);
+    for(i=0; i<n; i++){
         scanf("%d", &a);
-        res = a % 42;
-        unique.insert(res);
+        if(a==1){
+            scanf("%d", &p);
+            s.push(p);
+        } 
+        if(a==2){
+            if(!s.empty()) {
+                res.push_back(s.top());
+                //printf("%d\n", s.top());
+                s.pop();
+            } else {
+                res.push_back(-1);
+                //printf("-1\n");
+            }
+        } 
+        if(a==3){
+            //printf("%lu\n", s.size());
+            res.push_back(s.size());
+        } 
+        if(a==4){
+            if(s.empty()) {
+                // printf("%d\n", 1);
+                res.push_back(1);
+            } else {
+                res.push_back(0);
+                // printf("%d\n", 0);
+            }
+        } 
+        if(a==5){
+            if(!s.empty()) {
+                res.push_back(s.top());
+                //printf("%d\n", s.top());
+            }
+            else {
+                res.push_back(-1);
+                //printf("%d\n", -1);
+            }
+        }
+
+    } 
+    for(i=0; i<res.size(); i++){
+        printf("%d\n", res[i]);
     }
-    printf("%lu\n", unique.size());
     return 0;
 }
+
+
