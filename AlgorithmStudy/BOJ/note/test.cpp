587fef78436faf00614765300d6a0f4f73b122c9
seok-pyo <hongseokpyou@gmail.com>
2024-01-03 22:20:32 +0900

    기차운행 스택활용

--- AlgorithmStudy/BOJ/note/test.cpp
+++ AlgorithmStudy/BOJ/note/test.cpp
@@ -3,36 +3,28 @@
 #include <stack>
 #include <vector>
 using namespace std;
-
+int ch[1001];
+stack<int> S;
 int main(){
-    int k, i, j;
-    cin >> k;
-    vector<string> w(k);
-    vector<int> res(k);
-    for(i=0; i<k; i++){
-        bool isBal = true;
-        stack<char> S;
-        cin >> w[i];
-        for(j=0; j < w[i].length(); j++){
-            if(w[i][j]=='(') S.push(w[i][j]);
-            else if(w[i][j]==')') {
-                if(S.empty() || S.top() != '('){
-                    isBal = false;
-                    break;
-                }
+    int n, i, j, num, pos=1;
+    scanf("%d", &n);
+    for(i=1; i<=n; i++){
+        ch[i] = i;
+    }
+    for(i=0; i<n; i++){
+        scanf("%d", &num);
+        S.push(num);
+        while(!S.empty()){
+            if(S.top()==ch[pos]){
                 S.pop();
+                pos++;
+            } else {
+                break;
             }
         }
-        if(S.empty() && isBal) {
-            res[i] = 1;
-        } else {
-            res[i] = 0;
-        }
     }
 
-    for(i=0; i<k; i++){
-        if(res[i] == 1) printf("YES\n");
-        else printf("NO\n");
-    }
+    if(S.empty()) printf("Nice\n");
+    else printf("Sad\n");
     return 0;
 }
