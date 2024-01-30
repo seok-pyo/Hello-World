21b5ac59b5f6af2dc3c2717b064942ba4a7eeb43
seok-pyo <hongseokpyou@gmail.com>
2024-01-30 20:15:19 +0900

    clear

--- AlgorithmStudy/BOJ/note/test.cpp
+++ AlgorithmStudy/BOJ/note/test.cpp
@@ -1,43 +1,9 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
-#include <deque>
 using namespace std;
-vector<int> ball;
-deque<int> order;
+
 int main(){
-    int n, i, a, f, b;
-    scanf("%d", &n);
-    for(i=1; i<=n; i++){
-        scanf("%d", &a);
-        ball.push_back(a);
-        order.push_back(i);
-    }
-    int nf, nb;
-    f = order.front();
-    while(!order.empty()){
-        if(ball[f-1]>0){
-            for(i=0; i<ball[f-1]; i++){
-                if(i==0) {
-                    order.pop_front();
-                } else {
-                    nf = order.front();
-                    order.pop_front();
-                    order.push_back(nf);
-                } 
-            }
-        } else {
-            for(i=0; i<-ball[f-1]; i++){
-                if(i==0){
-                    order.pop_front();
-                } 
-                nb = order.back();
-                order.pop_back();
-                order.push_front(nb);
-            }
-        }
-        printf("%d ", f);
-        f = order.front();
-    }
+   
     return 0;
 }
 
