d1cd4c7216a2c0277176db5e091dc24df315c5fe
seok-pyo <hongseokpyou@gmail.com>
2024-05-10 13:33:32 +0900

    path

--- AlgorithmStudy/AA/practice/recursion.cpp
+++ AlgorithmStudy/AA/practice/recursion.cpp
@@ -1067,33 +1067,38 @@
 
 #include <iostream>
 using namespace std;
-int map[30][30], ch[30], cnt=0, n;
+int map[30][30], ch[30], cnt=0, n, path[30];
 
-void DFS(int v){
-    int i;
+void DFS(int v, int L){
+    int i, j;
     if(v==n){
         cnt++;
+        for(j = 0; j<L; j++){
+            cout << path[j] << ' ';
+        }
+        puts("");
     } else {
-        for(i = 1; i<=n; i++){
+        for(i=1; i<=n; i++){
             if(map[v][i]==1 && ch[i]==0){
                 ch[i] = 1;
-                DFS(i);
-                ch[i] = 0;
+                path[L] = i;
+                DFS(i, L+1);
+                ch[i]=0;
             }
         }
     }
 }
 
-
 int main(){
     int m, i, j, a, b, c;
     cin >> n >> m;
-    for(int i = 1; i<=m; i++){
+    for(int i=1; i<=m; i++){
         cin >> a >> b;
         map[a][b] = 1;
     }
-    //ch[1] = 1;
-    DFS(1);
+    ch[1]=1;
+    path[0]=1;
+    DFS(1, 1);
     cout << cnt << '\n';
     return 0;
 }
