de3cf9bcb64da5069da318836a3b777f5ab5dbb7
seok-pyo <hongseokpyou@gmail.com>
2024-05-25 23:44:42 +0900

    0525 재귀, 연결된 부분 찾기

--- AlgorithmStudy/AA/practice/recursion.cpp
+++ AlgorithmStudy/AA/practice/recursion.cpp
@@ -1624,35 +1624,77 @@
 // }
 
 // 선택 정렬
+// #include <iostream>
+// using namespace std;
+// int n;
+// int a[101];
+
+// int main(){
+//     cin >> n;
+//     for(int i = 0; i < n; i++){
+//         cin >> a[i];
+//     }
+//     int tmp = 0;
+//     for(int i = 0; i < n; i++){
+//         for(int j = i + 1; j < n; j++){
+//             if(a[i] > a[j]) {
+//                 tmp = a[j];
+//                 a[j] = a[i];
+//                 a[i] = tmp;
+//             }
+//         }
+//     }
+
+//     for(int k = 0; k < n; k++){
+//         cout << a[k] << ' ';
+//     }
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-int n;
-int a[101];
-
-int main(){
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> a[i];
-    }
-    int tmp = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = i + 1; j < n; j++){
-            if(a[i] > a[j]) {
-                tmp = a[j];
-                a[j] = a[i];
-                a[i] = tmp;
-            }
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+int m, n, k, y, x, ret, ny, nx, t;
+int a[51][51];
+bool visited[51][51];
+void dfs(int y, int x){
+    visited[y][x] = 1;
+    for(int i = 0; i < 4; i++){
+        ny = y + dy[i];
+        nx = x + dx[i];
+        if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+        if(a[ny][nx] == 1 && !visited[ny][nx]){
+            dfs(ny, nx);
         }
     }
-
-    for(int k = 0; k < n; k++){
-        cout << a[k] << ' ';
-    }
-
-    return 0;
+    return;
 }
 
-
+int main(){
+    cin >> t;
+    while(t--){
+        fill(&a[0][0], &a[0][0] + 51 * 51, 0);
+        fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
+        ret = 0;
+        cin >> m >> n >> k;
+        for(int i = 0; i < k; i++){
+            cin >> x >> y;
+            a[y][x] = 1;
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(a[i][j] == 1 && !visited[i][j]){
+                    dfs(i, j);
+                    ret++;
+                }
+            }
+        }
+        cout << ret << "\n";
+    }
+    return 0;
+}
 
 
 
