aa428c3ac14aa317ce1ec94a5d760b1bb3c1dc48
hongseokpyo <hongseokpyou@gmail.com>
2025-03-21 12:27:41 +0900

    이진법변환 반복하기(재귀함수 이진변환)

--- python/삼진법뒤집기.py
+++ python/삼진법뒤집기.py
@@ -23,3 +23,4 @@ def solution(n):
   
     
 print(solution(125))
+
---
+++ python/이진변환반복하기.py
@@ -0,0 +1,36 @@
+def solution(s):
+  answer=[0,0]
+  # 이진 변환 횟수
+  bitCnt=0
+  # 제거된 0의 개수
+  eliZero=0
+  # 0이 제거된 수
+  eliNum=s
+  comNum='0'
+  
+  while int(eliNum)!=1:
+    comNum=''
+    for num in eliNum:
+      if num=='0':
+        eliZero+=1
+      else:
+        comNum+='1'
+    eliNum=comNum
+    bitCnt+=1
+    eliNum=to_binary(int(len(eliNum)))
+  
+  
+  answer[0]=bitCnt
+  answer[1]=eliZero
+  return answer
+
+# 재귀함수를 활용한 이진법 변환
+def to_binary(n):
+  if n == 0:
+    return "0"
+  elif n == 1:
+    return "1"
+  else:
+    return to_binary(n//2) + str(n%2)
+    
+print(solution("110010101001"))
