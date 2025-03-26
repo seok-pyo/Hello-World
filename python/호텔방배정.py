80829aa298fa6d073a6193e7e789df10f9dc6733
hongseokpyo <hongseokpyou@gmail.com>
2025-03-26 12:17:20 +0900

    호텔방 배정4t

--- python/호텔방배정.py
+++ python/호텔방배정.py
@@ -1,20 +1,38 @@
+# def solution(k, room_number):
+#   hotel = {}
+#   answer = []
+#   def recur(n):
+#     if n not in hotel:
+#       hotel[n] = n + 1
+#       return n
+    
+#     empty_room = recur(hotel[n]) # 경로 압축
+#     hotel[n] = empty_room + 1 
+#     return empty_room
+  
+#   for room in room_number:
+#     assigned_room = recur(room)
+#     answer.append(assigned_room)
+  
+#   return answer
+
 def solution(k, room_number):
   hotel = {}
   answer = []
-  def recur(n):
+
+  def find(n):
     if n not in hotel:
-      hotel[n] = n + 1
-      return n
-    
-    empty_room = recur(hotel[n])
-    hotel[n] = empty_room + 1 # 경로 압축
-    return empty_room
-  for room in room_number:
-    assigned_room = recur(room)
-    answer.append(assigned_room)
+      hotel[n] = n
+    if hotel[n] != n:
+      hotel[n] = find(hotel[n])
+    return hotel[n]
   
+  for room in room_number:
+    assigned_room = find(room)
+    answer.append(assigned_room)
+    hotel[assigned_room] = assigned_room + 1
+
   return answer
 
-
 print(solution(10, [1,3,4,1,3,1]))
-    
+print(solution(1, [2,1]))
