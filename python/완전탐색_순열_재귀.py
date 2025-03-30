13e2d74e455844668e146db83810182d49b488a4
hongseokpyo <hongseokpyou@gmail.com>
2025-03-30 12:08:20 +0900

    완전탐색 순열 재귀 이해

--- python/완전탐색_순열_재귀.py
+++ python/완전탐색_순열_재귀.py
@@ -1,20 +1,16 @@
-# def permutation(arr, n):
-#   result = []
-#   if n == 0: return [[]]
-
-#   for (i, num) in enumerate(arr):
-#     for j in permutation(arr[:i] + arr[i + 1:], n -1):
-#       result.append([num] + j)
-#   return result
-
 def permutation(arr, n):
   result = []
-  if n == 0: return [[]]
+  if n == 0: 
+    print('----- s')
+    print(result)
+    print('----- e')
+    return [[]]
 
   for (i, num) in enumerate(arr):
-    for j in permutation(arr[:i] + arr[i+1:], n - 1):
+    for j in permutation(arr[:i] + arr[i + 1:], n - 1):
       result.append([num] + j)
+      print(result)
   return result
-  
-# print(permutation([1,2,3], 2))
-print([10] + [])
+
+print(permutation([1,2,3,4,5], 3))
+
