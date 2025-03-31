be3f97580154ebfdd3461013a65f87ca858abaf0
hongseokpyo <hongseokpyou@gmail.com>
2025-03-31 12:47:49 +0900

    조합 반복문 해석

--- python/완전탐색_조합_재귀.py
+++ python/완전탐색_조합_재귀.py
@@ -1,13 +1,24 @@
-# def combi(arr, r):
-#   result = []
-#   n = len(arr)
+def combi(arr, r):
+  result = []
+  n = len(arr)
 
-#   indices = list(range(r))
+  indices = list(range(r))
 
-#   while True:
-#     result.append([arr[i] for i in indices])
+  while True:
+    result.append([arr[i] for i in indices])
+  
+    increasing_index = None
 
-#     increasding_index = None
+    for i in reversed(range(r)):
+      # i + n - r 은 인덱스의 최대값
+      if indices[i] != i + n - r:
+        increasing_index = i
+        break
+    else:
+      return result
+  
+    indices[increasing_index] += 1
+    for j in range(increasing_index + 1, r):
+      indices[j] = indices[j - 1] + 1
 
-#     for i in reversed(range(r)):
-#       if indices[i] != i + n - r:
+print(combi([1,2,3,4,5], 3))
