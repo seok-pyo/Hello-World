785c3ad6e4f896a4817e781ea10d0f79c0a1f8b0
hongseokpyo <hongseokpyou@gmail.com>
2025-04-01 14:42:17 +0900

    완전탐색 조합 반복문 이해

---
+++ python/완전탐색_조합_반복문.py
@@ -0,0 +1,20 @@
+def combi(arr, r):
+  result = []
+  indices = list(range(r))
+
+  while True:
+    result.append([arr[i] for i in indices])
+    
+    for i in reversed(range(r)):
+      # i는 r의 범위를 나타내고, n - r은 r을 제외한 나머지, 따라서 i + len(arr) - r 은 n이 된다.
+      if indices[i] != i + len(arr) - r:
+        break
+    else:
+      return result
+    
+    indices[i] += 1
+    # i는 반드시 r보다 작은 수에서만 아래 반복문이 실행된다.
+    for j in range(i+1, r):
+      indices[j] = indices[j-1] + 1
+
+print(combi([1,2,3,4,5], 3))
