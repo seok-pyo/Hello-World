f5bab66c78a2d8d7d6d8ee4765aabad8995a9886
hongseokpyo <hongseokpyou@gmail.com>
2025-04-09 23:23:48 +0900

    완전탐색 불량사용자

--- python/완전탐색_불량사용자.py
+++ python/완전탐색_불량사용자.py
@@ -1,5 +1,22 @@
+import re
+from itertools import permutations 
+
+def is_match(user, banned):
+  pattern = '^' + banned.replace('*', '.') + '$'
+  return re.match(pattern, user)
+
 def solution(user_id, banned_id):
-  for i in user_id:
-    for j in banned_id:
-      if len(user_id[i]) != len(banned_id[j]): continue
-      
+  possible_case = set()
+
+  for perm in permutations(user_id, len(banned_id)):
+    match = True
+    for u, b in zip(perm, banned_id):
+      if not is_match(u, b):
+        match = False
+        break
+    if match:
+      possible_case.add(frozenset(perm))
+
+  return len(possible_case)
+  
+        
