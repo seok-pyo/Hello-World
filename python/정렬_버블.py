33028c8db0e8dbe4b55b8b6bdc3aade9f144f4a5
hongseokpyo <hongseokpyou@gmail.com>
2025-04-14 23:03:20 +0900

    선택 정렬

--- python/정렬_버블.py
+++ python/정렬_버블.py
@@ -1,10 +1,22 @@
 sample = [2,3,8,4,2,6,8,9,0]
+sample2 = [3, 0, 1, 8, 8, 7, 2, 5, 4, 6, 9]
 
-def bubbleSort(data):
+# def bubbleSort(data):
+#   for i in range(len(data) - 1):
+#     for j in range(i):
+#       if data[j] > data[j + 1]:
+#         data[j], data[j + 1] = data[j+1], data[j]
+
+# bubbleSort(sample2)
+# print(sample2)
+
+def bubble(data):
   for i in range(len(data) - 1):
-    for j in range(i):
+    for j in range(len(data) - 1 - i):
       if data[j] > data[j + 1]:
-        data[j], data[j + 1] = data[j+1], data[j]
-
-bubbleSort(sample)
+        data[j], data[j + 1] = data[j + 1], data[j]
+  
+bubble(sample)
 print(sample)
+
+
