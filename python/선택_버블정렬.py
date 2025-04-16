5ee1a166a93ed6d4a7239d188156aea7768fcf9d
hongseokpyo <hongseokpyou@gmail.com>
2025-04-16 13:13:09 +0900

    선택 정렬 이해

--- python/선택_버블정렬.py
+++ python/선택_버블정렬.py
@@ -12,12 +12,13 @@ print(bubble(sample))
 
 def selection(data):
   result = data[:]
-  for i in range(len(result) - 1):
+  for i in range(len(result) - 1): # 배열의 길이의 -1 만큼만 순회하는 것이 맞다.
     idx = i
-    for j in range(i + 1, len(result) - 1):
+    for j in range(i + 1, len(result)): # result 길이의 끝까지 돌아야 함으로
       if result[idx] > result[j]:
         idx = j
-    result[idx], result[i] = result[idx], result[i]
+    #result[idx], result[i] = result[idx], result[i] # 나열된 순서가 같으면 swap이 이뤄지지 않는다.
+    result[idx], result[i] = result[i], result[idx]
   return result
 
 print(selection(sample))
