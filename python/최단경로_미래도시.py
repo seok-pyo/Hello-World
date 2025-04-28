0a39f009f7cb7f21bd2ac7227c68ce6b6d31b2cf
hongseokpyo <hongseokpyou@gmail.com>
2025-04-28 14:07:59 +0900

    미래도시_최단경로

---
+++ python/최단경로_미래도시.py
@@ -0,0 +1,33 @@
+n, m = map(int, input().split())
+INF = 1e9
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(n + 1):
+    for j in range(n + 1):
+        if i == j:
+            graph[i][j] = 0
+
+for i in range(m):
+    row, col = map(int, input().split())
+    graph[row][col] = 1
+    # 양방향으로 가는 것의 비용이 동일하게 1이다. # 오답 참고!
+    graph[col][row] = 1
+
+x, k = map(int, input().split())
+
+for c in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][c] + graph[c][b])
+
+distance = graph[1][k] + graph[k][x]
+# 값이 무한대 보다 큰 경우(경로가 없는 경우 대응)
+
+if distance >= INF:
+    print('INFINITY')
+else:
+    print(graph[1][k] + graph[k][x])
+
+
+
+
