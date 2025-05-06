82aa0350d9dd8e84ab7bb2cd2283fff2230ae8da
hongseokpyo <hongseokpyou@gmail.com>
2025-05-06 16:50:41 +0900

    binary_search_practice

--- python/binary_search_test.py
+++ python/binary_search_test.py
@@ -1,19 +1,36 @@
 a = [3,7,6,2,4,6,8,9,1]
 
-def bs(array, target):
-    a.sort() # 수정
+# def bs(array, target):
+#     a.sort() # 수정
+#     start = 0
+#     end = len(array) - 1
+#     mid = (start + end) // 2
+
+#     while start != mid: # 수정
+#         for i in range(start, end): # for 불필요
+#             if array[i] == target:
+#                 return i
+#             if array[i] > target:
+#                 end = mid #
+#             else:
+#                 start = mid #
+
+# print(bs(a, 2))
+
+def bs(a, t):
+    a.sort()
     start = 0
-    end = len(array) - 1
-    mid = (start + end) // 2
+    end = len(a) - 1
 
-    while start != mid: # 수정
-        for i in range(start, end): # for 불필요
-            if array[i] == target:
-                return i
-            if array[i] > target:
-                end = mid #
-            else:
-                start = mid #
+    while start <= end:
+        mid = (start + end) // 2
+        if a[mid] == t:
+          return mid
+        if a[mid] > t:
+          end = mid - 1
+        else:
+          start = mid + 1
+    return -1
 
-print(bs(a, 2))
+print(bs(a, 4))
 
