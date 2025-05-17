42d0f71c9c15af92cf562c8a6dedf84b2dafe470
hongseokpyo <hongseokpyou@gmail.com>
2025-05-17 18:02:19 +0900

    selection sort

--- krafton-jungle/수_정렬하기.py
+++ krafton-jungle/수_정렬하기.py
@@ -7,3 +7,30 @@ def bubble_sort(a):
     return a
 
 print(bubble_sort([3,7,6,1,4,8,9,7,5]))
+
+def shaker_sort(a):
+    left = 0
+    right = len(a) - 1
+    last = right
+    while left < right:
+        for j in range(right, left, -1):
+            if a[j - 1] > a[j]:
+                a[j - 1], a[j] = a[j], a[j - 1]
+                last = j
+        left = last
+
+        for j in range(left, right):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                last = j
+        right = left
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        min = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min]:
+                min = j
+            arr[i], arr[min] = arr[min], arr[i]
+
