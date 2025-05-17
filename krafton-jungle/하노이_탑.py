3abca11856c44abe8ff384b34403be1d8dbe1c5d
hongseokpyo <hongseokpyou@gmail.com>
2025-05-17 13:54:47 +0900

    hanoi complete

--- krafton-jungle/하노이_탑.py
+++ krafton-jungle/하노이_탑.py
@@ -4,23 +4,20 @@ c = [0] * 100
 def hanoi(n, start, mid, end):
     if n == 0:
         return
-
     hanoi(n - 1, start, end, mid)
     print(start, end)
     hanoi(n - 1, mid, start, end)
 
 def count(n):
-    c[0] = 1
-    for i in range(1, n):
+    for i in range(0, n):
         c[i] = c[i - 1] * 2 + 1
 
-count(n)
-
 def sol(n):
-    if n < 20:
+    count(n)
+    if n <= 20:
         print(c[n - 1])
         hanoi(n, 1, 2, 3)
     else:
-        print(n * 2 + 1)
+        print(c[n - 1])
 
 sol(n)
