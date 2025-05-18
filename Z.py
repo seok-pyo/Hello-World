dc360180dd77d36c890fe970a9860c41a38e371f
hongseokpyo <hongseokpyou@gmail.com>
2025-05-18 20:17:48 +0900

    pass tc

--- Z.py
+++ Z.py
@@ -1,3 +1,7 @@
+import sys
+# sys.stdin = open("input.txt", "r")
+# 입력값 가져오기
+
 # n, r, c = map(int, input().split())
 
 # map = [[0] * ((2 ** n)) for _ in range((2 ** n))]
@@ -23,26 +27,61 @@
 
 # - - - - - - -
 
+# n, r, c = map(int, input().split())
+
+# ans, res = 0, 0
+
+# def z(n, x, y):
+#     global ans
+#     global res
+
+#     if int(y) == r and int(x) == c:
+#         res = ans
+#         return
+
+#     # if x < c and y < r:
+#     #     ans += 1
+#     #     return
+
+#     if n == 1:
+#         ans+=1
+#         return
+
+#     z(n/2, x, y)
+#     z(n/2, x + (n/2), y)
+#     z(n/2, x, y + (n/2))
+#     z(n/2, x + (n/2), y + (n/2))
+
+# z(2 ** n, 0, 0)
+# print(res)
+
+# - - - - - - -
+
 n, r, c = map(int, input().split())
 
-ans, res = 0, 0
+res, cnt = 0, 0
 
-def z(n, x, y):
-    global ans
+def z(n:int, x, y):
+    global cnt
     global res
 
-    if n == 1:
-        if int(y) == r and int(x) == c:
-            res = ans
-        ans+=1
+    if y == r and x == c:
+        print(cnt)
         return
 
-    z(n/2, x, y)
-    z(n/2, x + (n/2), y)
-    z(n/2, x, y + (n/2))
-    z(n/2, x + (n/2), y + (n/2))
+    if n == 1:
+        cnt+=1
+        return
+
+    if c < x and r < y:
+        return
+    if x + n <= c or y + n <= r: # 등호가 필요한 이유?
+        cnt+=int(n**2)
+    if x < c < x + n and y < r < y + n:
+        z(n//2, x, y)
+        z(n//2, x + (n//2), y)
+        z(n//2, x, y + (n//2))
+        z(n//2, x + (n//2), y + (n//2))
 
 z(2 ** n, 0, 0)
-print(res)
-
-
+# print(res)
