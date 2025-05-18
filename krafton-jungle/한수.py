9d747aa95504d608cf2e8b7770d40823d2a0b439
hongseokpyo <hongseokpyou@gmail.com>
2025-05-18 23:06:55 +0900

    한수

--- krafton-jungle/한수.py
+++ krafton-jungle/한수.py
@@ -1,15 +1,30 @@
-
 def sol():
     n = int(input())
     cnt = 0
-    if n < 100:
-        print(n)
-        return
 
-    sum = 0
+    e1 = 0
+    e2 = 0
+    e3 = 0
+    d1 = 0
+    d2 = 0
 
-    for d in range(1,5):
-        for i in range()
-    print(cnt)
+    for i in range(1, n + 1):
+        if len(str(i)) < 3:
+            cnt += 1
+        else:
+            e1 = i // 100
+            e2 = (i % 100) // 10
+            e3 = (i % 10) % 10
+            d1 = e1 - e2
+            d2 = e2 - e3
+            if d1 == d2: cnt+= 1
 
-sol()
+    return cnt
+
+print(sol())
+
+
+
+
+
+
