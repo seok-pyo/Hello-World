46e9cde635b177e3782f1993108c99e6ea2334e3
hongseokpyo <hongseokpyou@gmail.com>
2025-05-19 01:11:10 +0900

    merge sort

--- krafton-jungle/수_정렬하기_2.py
+++ krafton-jungle/수_정렬하기_2.py
@@ -1,32 +1,40 @@
 import sys
 input = sys.stdin.readline
 
-# n = int(input())
-# arr = []
+arr = [3,6,5,2,7,8,9]
+brr = [0] * len(arr)
 
-# for _ in range(n):
-#     arr.append(int(input()))
+def TopDownMergeSort(A, B, n):
+    CopyArray(A, 0, n, B) # duplicate array A into B
+    TopDownSplitMerge(B, 0, n, A) # sort data from B into A
 
-# def quick(arr):
-#     if len(arr) <= 1 :
-#         return arr
-#     pivot = arr[0]
-#     tail = arr[1:]
+# Sort the given run of array A using array B as a source.
+def TopDownSplitMerge(B, iBegin, iEnd, A):
+    if iEnd - iBegin < 2:
+        return # consider it sorted
 
-#     left = [e for e in tail if e < pivot]
-#     right = [e for e in tail if e > pivot]
+    iMiddle = (iEnd + iBegin) / 2
 
-#     return quick(left) + [pivot] + quick(right)
+    TopDownSplitMerge(A, iBegin, iMiddle, B) # sort the left run
+    TopDownSplitMerge(A, iMiddle, iEnd, B) # sort the right run
 
-# for i in quick(arr):
-#     print(i)
+    TopDownMerge(B, iBegin, iMiddle, iEnd, A)
 
-n = int(input())
-arr = []
-for _ in range(n):
-    arr.append(int(input()))
+def TopDownMerge(A, iBegin, iMiddle, iEnd, B):
+    i = iBegin
+    j = iMiddle
 
-arr.sort()
+    for k in range(int(i), int(j)):
+        if i < iMiddle and (j >= iEnd or A[int(i)] <= A[int(j)]):
+            B[k] = A[int(i)]
+            i = i + 1
+        else:
+            B[k] = A[int(j)]
+            j = j + 1
 
-for i in arr:
-    print(i)
+def CopyArray(A, iBegin, iEnd, B):
+    for k in range(iBegin, iEnd):
+        B[k] = A[k]
+
+TopDownMergeSort(arr, brr, len(arr))
+print(arr)
