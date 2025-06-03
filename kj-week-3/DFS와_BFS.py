7cc44c3405f7d248c37590fa250d44e991c23076
hongseokpyo <hongseokpyou@gmail.com>
2025-06-03 23:39:20 +0900

    DFS, BFS

--- kj-week-3/DFS와_BFS.py
+++ kj-week-3/DFS와_BFS.py
@@ -3,6 +3,8 @@ n, m, v = map(int, input().split())
 alist = [[] for _ in range(n + 1)]
 visited = [ False ] * (n + 1)
 
+dfs_list = []
+
 for _ in range(m):
     s, e = map(int, input().split())
     alist[s].append(e)
@@ -10,11 +12,35 @@ for _ in range(m):
 
 def dfs(alist, v, visited):
     visited[v] = True
-    print(v)
+    dfs_list.append(v)
 
     for i in sorted(alist[v]):
         if not visited[i]:
             dfs(alist, i, visited)
 
 dfs(alist, v, visited)
-# def bfs():
+print(*dfs_list)
+
+from collections import deque
+
+visited2 = [False] * (n + 1)
+bfs_list = []
+
+def bfs(alist, start, visited2):
+    queue = deque([start])
+
+    visited2[start] = True
+
+    while queue:
+        v = queue.popleft()
+        bfs_list.append(v)
+
+        for i in sorted(alist[v]):
+            if not visited2[i]:
+                queue.append(i)
+                visited2[i] = True
+
+bfs(alist, v, visited2)
+print(*bfs_list)
+
+
