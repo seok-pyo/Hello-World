d0c99336250a704746c29f3dc5e807baf2d4ca43
hongseokpyo <hongseokpyou@gmail.com>
2025-06-09 11:28:14 +0900

    LCS

--- kj-week-4/LCS.py
+++ kj-week-4/LCS.py
@@ -3,12 +3,19 @@ seq_2 = input()
 
 table = [[0] * (len(seq_1) + 1) for _ in range(len(seq_2) + 1)]
 
-for i, s1 in enumerate(seq_1):
-    for j, s2 in enumerate(seq_2):
+# for i, item in enumerate(seq_1):
+#     if seq_2[0] == item:
+#         table[0][i + 1] = 1
+# for j, item in enumerate(seq_2):
+#     if seq_1[0] == item:
+#         table[j + 1][0] = 1
+
+for i, s1 in enumerate(seq_1, start=1):
+    for j, s2 in enumerate(seq_2, start=1):
         if s1 == s2:
-            table[i][j] = max(table[i-1][j], table[i][j-1]) + 1
+            table[j][i] = table[j-1][i-1] + 1
         else:
-            table[i][j] = max(table[i-1][j], table[i][j-1])
+            table[j][i] = max(table[j][i-1], table[j-1][i])
 
-print(table[len(seq_1) -1][len(seq_2) - 1])
-
+# print(table)
+print(table[len(seq_2)][len(seq_1)])
