da62d45b1aab5aa06b4ec5d905995bfe8aaa38e6
hongseokpyo <hongseokpyou@gmail.com>
2025-10-24 14:32:58 +0900

    순열 사이클 1번째 시도

---
+++ 순열_사이클.py
@@ -0,0 +1,40 @@
+# 입력을 받아서 순열 사이클을 찾는 문제. 순열 사이클이란, 순열에 인덱싱을 해서 간선을 표현한 인접리스트 형식에서 시작점과 끝점이 같은 경우를 말한다.abs
+# 모든 원소를 반복문을 돌면서 BSF로 사이클이 있는지 확인하는 것
+
+from collections import deque
+
+t = int(input())
+
+for _ in range(t):
+    cnt = 0
+    graph_length = int(input())
+    graph = list(map(int, input().split()))
+    visi = [False] * (graph_length + 1)
+    q = deque()
+    q.append(1)
+
+    for i in range(1, graph_length + 1):
+        q.append(i)
+
+        while q:
+            if visi[i]:
+                continue
+            idx = q.popleft()
+            q.append(graph[idx-1])
+            if i == idx:
+                cnt += 1
+            visi[idx] = True
+
+    print(cnt)
+
+
+
+
+
+
+
+
+
+
+
+
