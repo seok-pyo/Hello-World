cbd9e3387d1a8bfc4738d0e99d11324c68db8552
hongseokpyo <hongseokpyou@gmail.com>
2025-10-30 22:19:39 +0900

    연결 요소의 개수

--- 연결_요소의_개수.py
+++ 연결_요소의_개수.py
@@ -1,30 +1,62 @@
+# import sys
+# from collections import deque
+
+# input = sys.stdin.readline
+# n, m = map(int, input().split())
+
+# # graph = [[] * (n + 1) for _ in range(n + 1)]
+# graph = [[] for _ in range(n + 1)]
+
+# for i in range(m):
+#     s, e = map(int, input().split())
+#     graph[s].append(e)
+#     graph[e].append(s)
+
+# visited = [False] * (n + 1)
+# result = 0
+
+# for start in range(1, n + 1):
+#     if visited[start]:
+#         continue
+#     result += 1
+#     q = deque([start])
+#     visited[start] = True
+
+#     while q:
+#         u = q.popleft()
+#         for v in graph[u]:
+#             if not visited[v]:
+#                 visited[v] = True
+#                 q.append(v)
+
+# print(result)
+
 import sys
-from collections import deque
+sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
 n, m = map(int, input().split())
 
 graph = [[] * (n + 1) for _ in range(n + 1)]
 
-for i in range(1, m):
+for i in range(m):
     s, e = map(int, input().split())
     graph[s].append(e)
     graph[e].append(s)
 
+
+def dfs(u):
+    visited[u] = True
+    for v in graph[u]:
+        if not visited[v]:
+            dfs(v)
+
 visited = [False] * (n + 1)
-
 result = 0
-d = deque([1])
+for i in range(1, n + 1):
+    if not visited[i]:
+        result += 1
+        dfs(i)
 
-while d:
-    for i in range(1, n + 1):
-        for j in range(len(graph[i])):
+print(result)
 
-    next = d.popleft()
-    if visited[next]:
-        continue
-
-    visited[next] = True
-    d.push(next)
-
-result+=1
