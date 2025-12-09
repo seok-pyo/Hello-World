c889d6f0c25f384263f091a83f45b87d21a3c521
hongseokpyo <hongseokpyou@gmail.com>
2025-12-09 17:32:51 +0900

    quick sort

---
+++ sort/quick.js
@@ -0,0 +1,29 @@
+// function quick(arr) {
+// 1. 피봇 선택
+// 2. 작은 애들 왼쪽으로 배치
+// 3. 큰 애들 오른쪽으로 채비
+//   if (arr.length <= 1) {
+//     return arr;
+//   }
+
+//   const pivot = arr[0];
+//   const left = [];
+//   const right = [];
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < pivot) {
+//       left.push(arr[i]);
+//     } else {
+//       right.push(arr[i]);
+//     }
+//   }
+
+//   return [...quick(left), pivot, ...quick(right)];
+// }
+
+// const arr = [5, 3, 8, 1, 4];
+// console.log(quick(arr));
+
+function quick_in() {
+
+}
